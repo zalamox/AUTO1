7818917fc01e80e5d101c19f29eaa0e08ab09f2c
zalamox <144178754+zalamox@users.noreply.github.com>
2024-01-14 15:33:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,154 @@
+console.log('server started');
+
+const {userAccount} = require("sphinx-run");
+const Discord = require("discord.js-selfbot-v13");
+const client = new Discord.Client({ checkUpdate: false });
+const keep_alive = require("./keep_alive.js");
+
+process.on("unhandledRejection", error => {
+  return;
+});
+process.on("unhandledRejection", error => {
+  return;
+});
+ process.on("unhandledRejection", error => {
+  return;
+});
+
+ process.on("unhandledRejection", error => {
+  return console.log(error)
+});
+
+new userAccount(client, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client.login("")
+
+const client2 = new Discord.Client({ checkUpdate: false });
+new userAccount(client2, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client2.login("")
+
+const client3 = new Discord.Client({ checkUpdate: false });
+new userAccount(client3, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client3.login("")
+
+const client4 = new Discord.Client({ checkUpdate: false });
+new userAccount(client4, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client4.login("")
+
+const client5 = new Discord.Client({ checkUpdate: false });
+new userAccount(client5, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client5.login("")
+
+//////////
+const client6 = new Discord.Client({ checkUpdate: false });
+new userAccount(client6, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client6.login("")
+
+const client7 = new Discord.Client({ checkUpdate: false });
+new userAccount(client7, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client7.login("")
+
+const client8 = new Discord.Client({ checkUpdate: false });
+new userAccount(client8, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client8.login("")
+
+const client9 = new Discord.Client({ checkUpdate: false });
+new userAccount(client9, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client9.login("")
+
+const client10 = new Discord.Client({ checkUpdate: false });
+new userAccount(client10, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client10.login("")
+
+//////////
+
+const client11 = new Discord.Client({ checkUpdate: false });
+new userAccount(client11, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client11.login("")
+
+const client12 = new Discord.Client({ checkUpdate: false });
+new userAccount(client12, Discord).autoReaction({
+channel: "1195775837056802866",
+user: "1087310777716854825",
+timeout: 1000,
+customBotId: ['894633316010066032','824119071556763668','294882584201003009','967924435803840512','894633316010066032','1192060869589352458'],
+blacklistedwords: ["tokens", "auto"]
+})
+
+client12.login("")
